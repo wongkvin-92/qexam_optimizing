673ff1242e4ab1288ab6b531bf613b9243e4a807
shath <shath.ibrahim@gmail.com>
2018-12-08 13:31:03 +0800

    Input changed

--- NetBeansProjects/Qexam/src/qexam/Qexam.java
+++ NetBeansProjects/Qexam/src/qexam/Qexam.java
@@ -15,24 +15,53 @@ public class Qexam {
     static Scanner qq = new Scanner(System.in);
     
     /**
+     * Display the main menu
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) {        
         boolean done = false;
         do
         {
             int response = doMenu();
             switch (response)
             {
-                case 1: addPreference();	break;//VehicleDialog 
-                case 2: insertDate(); break;//do
-                case 3: autoPlanned(); break;
-                case 4: done = true; break;
+                case 1: addStaff(); break;//
+                case 2: preferenceMenu(); break;
+                case 3: assignEmployee(); break;                
+                case 4: viewAssignments(); break;                               
+                case 5: autoPlanned(); break;
+                case 0: done = true; break;
                 default: displayErrorMsg(); break;
             }
             System.out.println(); // skip a blank line
         } while (!done);
+    }
+    
+    /**
+     * Display the preference menu
+     */
+    public static void preferenceMenu(){
+        boolean done=false;;
+        do{
+            int response = doPreferenceMenu();
+            switch(response){
+                case 0: done= true; break;                
+                case 1: addPreference(); break;
+                case 2: deletePreference(); break;
+                default:
+                    displayErrorMsg(); break;
+            }        
+            System.out.println();
+        }while(!done);
+    }
+    
+    public static int doPreferenceMenu(){
+        System.out.println("1. Add Preference");
+        System.out.println("2. Delete Preference");  
+        System.out.println("0. Back");
+        System.out.println();
+        System.out.println("Your choice: ");
+        return qq.nextInt();       
     }
     
         /**
@@ -42,44 +71,44 @@ public class Qexam {
     public static int doMenu()
     {
         System.out.println("The Magic of Planning and Scheduling in the Logistic World");
-        System.out.println("1. Add Staff Preference");//done
-        System.out.println("2. Mannually insert date");//done
-        System.out.println("3. Run Automatic Planning");//done
-        System.out.println("4. Quit");
+        System.out.println("1. Add Staff");//done
+        System.out.println("2. Configure Preference");
+        System.out.println("3. Assign Employee to day");
+        System.out.println("4. View Assignments");
+        System.out.println("5. Generate Optimal Schedule");
+        System.out.println("6. View a list of employees");
+        System.out.println("0. Quit");
         System.out.println();
         System.out.print("Your choice: ");
         return qq.nextInt();
     }
     
+    public static void addStaff(){
+        String name;
+        System.out.println("Enter name");
+        name = qq.next();
+        Employee employee = new Employee(name);
+        System.out.println("Employee created successfully");
+        System.out.println(employee);
+    }
+       
     public static void addPreference()
     {
-        int count = 1;
-        do{
-            /*
-             * 
-            */
-            System.out.println("What is the day that student " + count + " not allowed to work?");
-            int dayNoWork = qq.nextInt();
-            if(dayNoWork > 7){
-                System.out.println("Please key in within 1 -7 (Mon-Sun)");
-            }
-            new Employee();
-           // new Employee(dayNoWork);
-             
-        /*    System.out.println( count);
-            count+=1; */
-        }while(count !=6);
-       
-    }
+        System.out.println("Enter student id: ");
+        String studentID = qq.next();
+        System.out.println("Enter day[1-14 or *]:");
+        String dayStr = qq.next();
+        System.out.println("Is student allowed to work on this day? (y/n):");
+        String yesOrNo = qq.next();
+        System.out.println("Enter penalty: ");
+        double penalty = qq.nextDouble();
         
-    public static void insertDate()
-    {
-        System.out.println("Invalid Choice. Try Again!");
+        
     }
     
         public static void autoPlanned()
     {
-        System.out.println("Invalid Choice. Try Again!");
+        System.out.println("Not implemented yet");
     }
         /**
      * Method that returns an error message when user has entered
@@ -89,4 +118,17 @@ public class Qexam {
     {
         System.out.println("Invalid Choice. Try Again!");
     }
+
+    private static void assignEmployee() {
+        System.out.println("Not Implemented yet");
+    }
+
+    private static void viewAssignments() {
+        System.out.println("Not implemented yet");
+    }
+
+    private static void deletePreference() {
+        System.out.println("Not implemented yet");
+    }
+
 }
