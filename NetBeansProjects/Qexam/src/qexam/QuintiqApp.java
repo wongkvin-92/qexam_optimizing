dc6a69003de77a8c02cf246ae378335d221dadaa
wongkvin-92 <wkvin0704@gmail.com>
2018-12-08 15:04:51 +0800

    Constraint model renamed to Preference

--- NetBeansProjects/Qexam/src/qexam/QuintiqApp.java
+++ NetBeansProjects/Qexam/src/qexam/QuintiqApp.java
@@ -4,39 +4,39 @@
  * and open the template in the editor.
  */
 package qexam;
-import java.util.ArrayList;
+import java.util.HashMap;
 /**
  *
  * @author wKvin
  */
 public class QuintiqApp {
-    private ArrayList<Employee> employees;
-    private ArrayList<Preference> preferences;
+    private HashMap<Integer, Employee> employees;
+    private HashMap<Integer, Preference> preferences;
     
     public QuintiqApp(){
-        setEmployees(new ArrayList<Employee>());
-        setPreferences(new ArrayList<Preference>());
+        setEmployees(new HashMap<>());
+        setPreferences(new HashMap<>());
 }
     
-    public ArrayList<Employee> getEmployees(){
+    public HashMap<Integer, Employee> getEmployees(){
         return employees;
     }
     
-    public void  setEmployees(ArrayList<Employee> employees){
+    public void  setEmployees(HashMap<Integer, Employee> employees){
         this.employees = employees;
     }
     
-    public ArrayList<Preference> getPreferences(){
+    public HashMap<Integer,Preference> getPreferences(){
         return preferences;
     }
     
-    public void setPreferences(ArrayList<Preference> preferences){
+    public void setPreferences(HashMap<Integer,Preference> preferences){
         this.preferences = preferences;
     }
     
     public Employee createEmployee(String name){
         Employee emp = new Employee(name);
-        employees.add(emp.getID(), emp);
+        employees.put(emp.getID(), emp);
         return emp;
     }
     
@@ -60,7 +60,7 @@ public class QuintiqApp {
     
     public Preference createPreference(int id, String weekDay, boolean allowed, int penalty){
         Preference pre = new Preference(id, weekDay, allowed, penalty);
-        preferences.add(pre.getPreferenceId(), pre);
+        preferences.put(pre.getPreferenceId(), pre);
         return pre;
     }
     public void deletePreference(int id){
